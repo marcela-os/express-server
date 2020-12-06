022c1ec88b01367155f1f07f65be300d383571bf
Marcelina <m.oscislowska@gmail.com>
2020-12-06 17:48:59 +0100

    Add views

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+app.get('/' || '/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+  });
